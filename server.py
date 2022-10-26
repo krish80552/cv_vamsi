6b264ef00dd8c0d7147545bf3d2e3ac97db77785
krish80552 <94895903+krish80552@users.noreply.github.com>
2022-10-26 17:26:16 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/cv_vamsi/')
+@app.route('/')
 def hello_world():
     return render_template('index.html')
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="https://krish80552.github.io/cv_vamsi")
